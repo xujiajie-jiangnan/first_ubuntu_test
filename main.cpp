ba3b439e52d5b1f3a42b933e1065ef8a2a017453
xujiajie <15852787969@163.com>
2021-12-08 05:41:12 -0800

    master

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(int argc,char** argv)
+{
+    vector<int> v(10);
+    int a=10,b=20;
+    //genggaide 
+    double c=10.0;
+    cout<<"hhh"<<endl;
+    return 0;
+}
